6bc24ce338f8f3ba7aa7fa96dfd30a7957036ae8
Atharva Amdekar <aamdekar@stanford.edu>
2022-10-18 19:29:51 -0700

    Added blocked implementation

--- part_a/tasksys.cpp
+++ part_a/tasksys.cpp
@@ -54,10 +54,19 @@ TaskSystemParallelSpawn::TaskSystemParallelSpawn(int num_threads): ITaskSystem(n
     // Implementations are free to add new class member variables
     // (requiring changes to tasksys.h).
     //
-    
+    n_threads = num_threads;
 }
 
 TaskSystemParallelSpawn::~TaskSystemParallelSpawn() {}
+
+void TaskSystemParallelSpawn::dowork(IRunnable* runnable, int block_num, int num_total_tasks) {
+    // int task_id = n_threads * block_num + std::this_thread::get_id();
+    int mintask = n_threads * block_num;
+    int maxtask = mintask + n_threads;
+    for(int i =mintask; i < maxtask; i++) {
+        runnable->runTask(i, num_total_tasks);
+    }
+}
 
 void TaskSystemParallelSpawn::run(IRunnable* runnable, int num_total_tasks) {
 
@@ -67,20 +76,30 @@ void TaskSystemParallelSpawn::run(IRunnable* runnable, int num_total_tasks) {
     // method in Part A.  The implementation provided below runs all
     // tasks sequentially on the calling thread.
     //
-    workers = new std::thread[num_total_tasks];
-    for (int i = 0; i < num_total_tasks; i+=n_threads) {
-        for (int j = i; j < i + n_threads; j++) {
-            workers[j] = std::thread(&IRunnable::runTask, runnable, j, num_total_tasks);
-        }
-        for (int j = i; j < i + n_threads; j++) {
-            workers[j].join();
-        }
-    }
-    
 
-    // for (int i = 0; i < num_total_tasks; i++) {
-    //     runnable->runTask(i, num_total_tasks);
+    // interleaved assignment gives not ok
+    // workers = new std::thread[num_total_tasks];
+    // for (int i = 0; i < num_total_tasks; i+=n_threads) {
+    //     for (int j = i; j < i + n_threads; j++) {
+    //         workers[j] = std::thread(&IRunnable::runTask, runnable, j, num_total_tasks);
+    //     }
+    //     for (int j = i; j < i + n_threads; j++) {
+    //         workers[j].join();
+    //     }
     // }
+    
+    // blocked assignment
+    workers = new std::thread[n_threads];
+    int n_blocks = num_total_tasks/n_threads;
+    for (int i = 0; i < n_blocks; i++) {
+        workers[i] = std::thread(&TaskSystemParallelSpawn::dowork, this, runnable, i, num_total_tasks);
+    }
+    for (int i = 0; i < n_blocks; i++) {
+        workers[i].join();
+    }
+    for (int i = 0; i < num_total_tasks; i++) {
+        runnable->runTask(i, num_total_tasks);
+    }
 }
 
 TaskID TaskSystemParallelSpawn::runAsyncWithDeps(IRunnable* runnable, int num_total_tasks,
--- part_a/tasksys.h
+++ part_a/tasksys.h
@@ -2,6 +2,7 @@
 #define _TASKSYS_H
 
 #include "itasksys.h"
+#include<thread>
 
 /*
  * TaskSystemSerial: This class is the student's implementation of a
@@ -34,6 +35,10 @@ class TaskSystemParallelSpawn: public ITaskSystem {
         TaskID runAsyncWithDeps(IRunnable* runnable, int num_total_tasks,
                                 const std::vector<TaskID>& deps);
         void sync();
+    private:
+        int n_threads;
+        std::thread* workers;
+        void dowork(IRunnable* runnable, int block_num, int num_total_tasks);
 };
 
 /*
